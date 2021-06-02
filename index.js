53530ea604e0d473dd6f1c4badd7988dd5abccc2
MK <mihaelak08@gmail.com>
2021-06-02 09:08:46 -0700

    schema and js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+const inquirer = require("inquirer");
 
+function mainMenu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "MAIN MENU",
+      choices: [
+        "View all employees",
+        "View all employees by role",
+        "View all employees by department",
+        "Add employee",
+        "Add role",
+        "Add department",
+        "Update employee role",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "View all employees":
+          allEmp();
+          break;
+        case "View all employees by role":
+          allRole();
+          break;
+        case "View all employees by department":
+          allDept();
+          break;
+        case "Add employee":
+          addEmp();
+          break;
+        case "Add role":
+          addRole();
+          break;
+        case "Add department":
+          addDept();
+          break;
+        case "Update employee role":
+          updateEmpRole();
+          break;
+        case "End":
+          connection.end();
+          break;
+      }
+    });
+}
