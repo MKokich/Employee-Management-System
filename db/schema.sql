da4b6665b121eb13d0f87d7d9723a7d82acfed19
MK <mihaelak08@gmail.com>
2021-06-02 10:34:22 -0700

    working on js and localhost problem

--- db/schema.sql
+++ db/schema.sql
@@ -26,5 +26,92 @@ CREATE TABLE employee (
     manager_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
+
+-----------------------
+
+INSERT INTO department (id, name)
+VALUES (1, "Marketing Department");
+
+INSERT INTO department (id, name)
+VALUES (2, "Shipping Department");
+
+INSERT INTO department (id, name)
+VALUES (3, "International Department");
+
+-----------------------
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Manager", 90000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (2, "Assistant Manager", 85000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (3, "Marketing Director", 100000, 1);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (4, "Shipping Director", 90000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (5, "Manager", 75000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (6, "Assistant Manager", 70000, 2);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (7, "International Director", 200000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (8, "Manager", 180000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "International Leison", 175000, 3);
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (10, "Assistant Manager", 170000, 3);
+
+------------------------
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Jack", "Dobbs", 7);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "Tim", "Swiss", 6);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Jessica", "Keppner", 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "Adam", "Rollins", 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "John", "Smith", 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Lily", "Thompson", 5);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Dwight", "Aldrich", 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Megan", "Hanson", 10);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Barry", "White", 6);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "Jackson", "Monroe", 9);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (2, "Alyssa", "Hart", 2);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (1, "Kim", "Knolls", 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Veronica", "Perry", 8);
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+VALUES (3, "Mackenzie", "Nelson", 10);
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const inquirer = require("inquirer");
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: process.env.password,
+  database: "employee_db",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("\n Employee Management System \n");
+  mainMenu();
+});
 
 function mainMenu() {
   inquirer
     .prompt({
-      name: "action",
+      name: "choice",
       type: "list",
       message: "MAIN MENU",
       choices: [
@@ -16,8 +35,9 @@ function mainMenu() {
         "Update employee role",
       ],
     })
+
     .then((answer) => {
-      switch (answer.action) {
+      switch (answer.choice) {
         case "View all employees":
           allEmp();
           break;
@@ -45,3 +65,229 @@ function mainMenu() {
       }
     });
 }
+
+const allEmp = () => {
+  const query =
+    "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+  connection.query(query, function (err, data) {
+    if (err) throw err;
+    console.table(data);
+    mainMenu();
+  });
+};
+
+const allDept = () => {
+    const query =
+      "SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;";
+    connection.query(query, function (err, data) {
+      if (err) throw err;
+      console.table(data);
+      mainMenu();
+    });
+  },
+  allRole = () => {
+    const query =
+      "SELECT employee.first_name, employee.last_name, role.title AS Title FROM employee JOIN role ON employee.role_id = role.id;";
+    connection.query(query, function (err, data) {
+      if (err) throw err;
+      console.table(data);
+      mainMenu();
+    });
+  },
+  addEmp = () => {
+    connection.query("SELECT * FROM role", function (err, data) {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name? ",
+          },
+          {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name? ",
+          },
+          {
+            name: "role",
+            type: "list",
+            choices: function () {
+              var roleList = [];
+              for (let i = 0; i < data.length; i++) {
+                roleList.push(data[i].title);
+              }
+              return roleList;
+            },
+            message: "What is the employee's role? ",
+          },
+          {
+            type: "input",
+            message: "What is the manager id number?",
+            name: "managerID",
+          },
+        ])
+        .then(function (answer) {
+          let roleID;
+          for (let x = 0; x < data.length; x++) {
+            if (data[x].title == answer.role) {
+              roleID = data[x].id;
+              console.log(roleID);
+            }
+          }
+          let managerID;
+          for (let k = 0; k < data.length; k++) {
+            if (data[k].title == answer.manager) {
+              managerID = data[k].id;
+              console.log(managerID);
+            }
+          }
+
+          connection.query(
+            "INSERT INTO employee SET ?",
+            {
+              first_name: answer.first_name,
+              last_name: answer.last_name,
+              role_id: roleID,
+              manager_id: answer.managerID,
+            },
+            (err) => {
+              if (err) throw err;
+              console.log(
+                `Successfully added ${answer.first_name} ${answer.last_name} to Employees`
+              );
+              mainMenu();
+            }
+          );
+        });
+    });
+  };
+
+addDept = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the Department you would like to add?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query("INSERT INTO department SET ?", {
+        name: answer.newDepartment,
+      });
+
+      console.log(
+        `${answer.newDepartment} has successfully been added to Departments`
+      );
+      mainMenu();
+    });
+};
+
+addRole = () => {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "newRole",
+        message: "What's the name of this role?",
+      },
+      {
+        type: "input",
+        name: "addSal",
+        message: "What is the salary for this role?",
+      },
+      {
+        type: "input",
+        name: "deptId",
+        message: "What is the department id number?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query("INSERT INTO role SET ?", {
+        title: answer.newRole,
+        salary: answer.addSal,
+        department_id: answer.deptId,
+      });
+
+      console.log(`${answer.newRole} has successfully been added to Roles`);
+      mainMenu();
+    });
+};
+addRole = () => {
+  connection.query(
+    "SELECT role.title AS Title, role.salary AS Salary FROM role",
+    function (err, res) {
+      inquirer
+        .prompt([
+          {
+            name: "title",
+            type: "input",
+            message: "What is the title of this role",
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "What is the salary of this role?",
+          },
+          {
+            name: "deptId",
+            type: "input",
+            message: "What is the department id number?",
+          },
+        ])
+        .then(function (answer) {
+          connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: answer.title,
+              salary: answer.salary,
+              department_id: answer.deptId,
+            },
+            (err) => {
+              if (err) throw err;
+              console.log(
+                `${answer.title} has successfully been added to Roles`
+              );
+              mainMenu();
+            }
+          );
+        });
+    }
+  );
+};
+
+updateEmpRole = () => {
+  inquirer
+    .prompt([
+      {
+        name: "pickEmp",
+        type: "input",
+        message: "What is the ID number of the employee you'd like to update?",
+      },
+      {
+        name: "updateRole",
+        type: "input",
+        message: "What is the employees new role ID?",
+      },
+    ])
+    .then(function (answer) {
+      connection.query(
+        "UPDATE employee SET ? WHERE ?",
+        [
+          {
+            role_id: answer.updateRole,
+          },
+          {
+            id: answer.pickEmp,
+          },
+        ],
+        (err) => {
+          if (err) throw err;
+          console.log("Employee's role has sucessfully been updated");
+          mainMenu();
+        }
+      );
+    });
+};
